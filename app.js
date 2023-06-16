ef221cd50dc2cc3044f5a93c84d898be9eeb5018
di1ara <dilarayalniz1@gmail.com>
2023-06-16 16:37:52 -0400

    changes to push

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const backgroundMusic = document.getElementById('background-music');
-    backgroundMusic.play();
+const backgroundMusic = new Audio('/audio/music.mp3'); // Replace 'path/to/your/music.mp3' with the actual path to your music file
+
+const musicButton = document.getElementById('music-button');
+let isMusicPlaying = false;
+
+musicButton.addEventListener('click', function() {
+    if (isMusicPlaying) {
+        backgroundMusic.pause();
+        isMusicPlaying = false;
+        musicButton.classList.remove('playing');
+    } else {
+        backgroundMusic.play();
+        isMusicPlaying = true;
+        musicButton.classList.add('playing');
+    }
 });
-
-function pauseBackgroundMusic() {
-    const backgroundMusic = document.getElementById('background-music');
-    backgroundMusic.pause();
-}
-
-window.addEventListener('beforeunload', pauseBackgroundMusic);
-
